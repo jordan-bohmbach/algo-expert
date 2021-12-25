82b5ddec12470628d4965224b39aa2471556620f
Jordan Bohmbach <jordan.bohmbach@gmail.com>
2021-12-25 13:23:41 -0600

    continued studying

--- Medium/Min Number of Coins for Change/min-number-of-coins-for-change.js
+++ Medium/Min Number of Coins for Change/min-number-of-coins-for-change.js
@@ -1,21 +1,19 @@
 function minNumberOfCoinsForChange(n, denoms) {
-    let targetAmounts = [0]
-    for (let i = 0; i < n; i++) {
-        targetAmounts.push(Infinity)
-    }
+    let minConfigurations = new Array(n + 1).fill(Infinity)
+    minConfigurations[0] = 0
 
-    denoms.forEach(denom => {
-        targetAmounts.forEach((target, i) => {
-            if (i >= denom) {
-                let newNum = Math.min(Math.floor(i / denom) + targetAmounts[i % denom], targetAmounts[i - denom]+1)
-                if (newNum < targetAmounts[i]) targetAmounts[i] = newNum
+    denoms.forEach(coinSize => {
+        for (let i = 1; i < minConfigurations.length; i++) {
+            if (i - coinSize >= 0) {
+                minConfigurations[i] = Math.min(minConfigurations[i], minConfigurations[i - coinSize] + 1)
             }
-        })
+        }
     })
-    if (targetAmounts[targetAmounts.length - 1] < Infinity) return targetAmounts[targetAmounts.length - 1]
+    if (minConfigurations[minConfigurations.length - 1] !== null) return minConfigurations[minConfigurations.length - 1]
     return -1
 }
 
 // console.log(minNumberOfCoinsForChange(7, [1,5,10]))
-console.log(minNumberOfCoinsForChange(3, [2 , 1]))
-// console.log(minNumberOfCoinsForChange(7, [2, 4]))
+// console.log(minNumberOfCoinsForChange(3, [2 , 1]))
+console.log(minNumberOfCoinsForChange(7, [2, 4]))
+// console.log(minNumberOfCoinsForChange(6, [2, 3]))
